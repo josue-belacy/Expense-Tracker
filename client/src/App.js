2eef52678e13f533ec2a02d123a197d2ef78d7ec
Josue Belacy <jbelacy@gmail.com>
2022-05-24 02:54:29 -0400

    Folder Structure, NPM Modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css'
+import {Button} from 'antd'
 
 function App() {
   return (
     <div className="App">
       <h1>Expense Tracking App</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
