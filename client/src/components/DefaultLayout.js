94a98ae5fe63bbee349553b9e0c734d04aa87c58
Josue Belacy <jbelacy@gmail.com>
2022-05-24 04:34:08 -0400

    Default Layout applied across all pages

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../resources/default-layout.css';
 
 function DefaultLayout(props) {
-  return (
-    <div className='layout'>
-      
-        <div className='header'>
-          <div>
-            <h1 className='logo'>Expense Tracker</h1>
-          </div>
-        </div>
+	return (
+		<div className="layout">
+			<div className="header d-flex justify-content-between align-items-center">
+				<div>
+					<h1 className="logo">Expense Tracker</h1>
+				</div>
+				<div>
+					<h1 className="username">Username</h1>
+				</div>
+			</div>
 
-        <div className='content'>
-          {props.children}
-        </div>
-
-    </div>
-  )
+			<div className="content">{props.children}</div>
+		</div>
+	);
 }
 
-export default DefaultLayout
+export default DefaultLayout;
